f2fc106ea4357db7ad63372c3110ddc9e2debcc1
fumimi <info@fumimi.jp>
2017-02-01 21:48:38 +0900

    次の画面のボタンを押すとラベルのテキストが変更される。

--- SwiftButton/NextViewController.swift
+++ SwiftButton/NextViewController.swift
@@ -9,27 +9,36 @@
 import UIKit
 
 class NextViewController: UIViewController {
+    
+    
+    @IBOutlet var changeLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    
+    @IBAction func change(_ sender: AnyObject) {
+        
+        changeLabel.text = "暗号が解除されました。"
+
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+        
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
